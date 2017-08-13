43620ca72c48a7ec39ef134f0c32a783609d5344
rishabh <user1afresno@gmail.com>
2017-08-13 10:30:43 -0700

    Adding Age, Second Commit

--- Sample1/src/src/PrintHello.java
+++ Sample1/src/src/PrintHello.java
@@ -3,8 +3,12 @@ package src;
 public class PrintHello {
 	public void display(){
 		displayName();
+		displayAge(); 
 	}
 	private void displayName(){
 		System.out.println("Rishabh Sharma");
+	}
+	private void displayAge(){
+		System.out.println("I am 22 years old");
 	}
 }
