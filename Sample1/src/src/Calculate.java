68663b76cd5e4792bef1f5e6cf83445523562cb2
rishabh <user1afresno@gmail.com>
2017-08-13 12:10:08 -0700

    adding division fuctionality

--- Sample1/src/src/Calculate.java
+++ Sample1/src/src/Calculate.java
@@ -6,5 +6,10 @@ public class Calculate {
 		System.out.println("addition of "+i+ " and " +j+ " = " +(i+j) );
 		
 	}
-
+	public void mul(int i,int j) {
+		System.out.println("multiplication of "+i+ " and " +j+ " = " +(i*j) );
+	}
+	public void div(int i,int j){
+		System.out.println("Division of "+i+ " and " +j+ " = " +(i/j) );
+	}
 }
