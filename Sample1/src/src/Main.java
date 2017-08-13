125f557165a71d4d327e6c87e9e724b3c812c899
rishabh <user1afresno@gmail.com>
2017-08-13 10:16:46 -0700

    first commit

---
+++ Sample1/src/src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PrintHello print = new PrintHello();
+		print.display();
+
+	}
+
+}
---
+++ Sample1/src/src/PrintHello.java
@@ -0,0 +1,10 @@
+package src;
+
+public class PrintHello {
+	public void display(){
+		displayName();
+	}
+	private void displayName(){
+		System.out.println("Rishabh Sharma");
+	}
+}
